c8c62ef3965e6f81a65b378ebe96f9a8cb087380
Jan Bölsche <regular.gonzales@googlemail.com>
2015-04-05 18:17:16 +0200

    added spinner, refactoring: moved episode-related code into lib/episode.js

--- index.js
+++ index.js
@@ -1,52 +1,37 @@
 var EventEmitter = require('events').EventEmitter;
 window.events = new EventEmitter();
 
-var history = require('./history')('.sidebar .history ul');
-var topbar = require('./topbar')('.topbar');
-var discover = require('./discover');
-var feedback = require('./feedback');
+var history = require('./lib/history')('.sidebar .history ul');
+var topbar = require('./lib/topbar')('.topbar');
+var discover = require('./lib/discover');
+var episode = require('./lib/episode');
 var getMenuHTML = require('./lib/menu').getMenuHTML;
 
-var getEpisodeURL = require('./urls').getEpisodeURL;
-var appendScriptTag = require('./jsonp');
+var Spinner = require('spin');
 
 var TOC = {};
 var TRACKS = process.env.tracks.split(':');
-console.log('tracks', TRACKS);
-
 
 window.events.once('end_episode_discovery', function(toc) {
     TOC = toc;
     console.log('episode discovery done.');
-    window.events.on('append_episode', function(track, pkg, html) {
-        console.log('appending episode');
-        var container = document.querySelector('.content');
-
-        var div = document.createElement('div');
-        div.classList.add('episode');
-        div.setAttribute('name', pkg.name);
-        div.innerHTML = html;
-        container.appendChild(div);
-        feedback(div, function(r) {
-            history.appendEpisode(toc[pkg.name]);
-            var menuHTML = getMenuHTML(toc, [], [], TRACKS, 4); 
-            //console.log(menuHTML);
-            div.innerHTML += menuHTML;
-        });
-    });
     loadAndAppendEpisode('intro');
-    loadAndAppendEpisode('tty');
-    loadAndAppendEpisode('terminal-emulator');
+    //loadAndAppendEpisode('tty');
+    //loadAndAppendEpisode('terminal-emulator');
 });
 
 window.events.on('history_clicked', function(episode) {
     console.log('history click on', episode.pkg.name);
 });
 
+window.events.on('finished_episode', function(model) {
+    history.appendEpisode(model);
+    var menuHTML = getMenuHTML(TOC, [], [], TRACKS, 4); 
+    var div = document.querySelector('.episode[name='+ model.pkg.name +']');
+    div.innerHTML += menuHTML;
+});
 
 function loadAndAppendEpisode(name) {
-    var url = getEpisodeURL('shecodes-content', name);
-    appendScriptTag(url);
+    var container = document.querySelector('.content');
+    episode.appendEpisode(container, TOC[name]);
 }
-
-
