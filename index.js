90e95e683e74236eb338fd3ba27b10db4de7cdc8
Jan Bölsche <regular.gonzales@googlemail.com>
2015-04-25 16:32:22 +0200

    autoscroll, adding episodes to history immediatly after displaying them (closing #19 and #11)

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ var appendMenu = require('./lib/menu').appendMenu;
 var bpm = require('brainpm');
 var Spinner = require('spin');
 
-var scrollToY = require('scroll-to-y');
+var scrollToY = function(y) {
+    require('scroll-to-y')(y, 1500, 'easeInOutQuint');
+};
 
 var Spinner = require('spin');
 
@@ -48,18 +50,39 @@ window.events.once('end_episode_discovery', function(toc) {
     //loadAndAppendEpisode('terminal-emulator');
 });
 
+function scrollToEpisode(name) {
+    var el = document.querySelector('.episode[name=' + name + ']');
+    var ypos = el.offsetTop;
+    scrollToY(ypos); 
+}
+
 window.events.on('history_clicked', function(episode) {
     console.log('history click on', episode.pkg.name);
-    var el = document.querySelector('.episode[name=' + episode.pkg.name + ']');
-    var ypos = el.offsetTop;
-    scrollToY(ypos, 1500, 'easeInOutQuint');
+    scrollToEpisode(episode.pkg.name);
 });
 
+function ensureVisible(el) {
+    var bounds = el.getBoundingClientRect();
+    // if top is negative, we need to scroll down
+    // if top + height > viewport.height, we need to scroll up
+    // if both is true, we scroll up
+    console.log('ENSURE',el,bounds);
+    var clientHeight = document.querySelector('html').clientHeight;
+    console.log(clientHeight);
+    var deltaBottom = bounds.bottom - clientHeight;
+    console.log('delta', deltaBottom);
+    if (deltaBottom > 0) {
+        scrollToY(window.scrollY + deltaBottom);
+    } else if (bounds.top < 0) {
+        scrollToY(window.scrollY + boundsTop);
+    }
+}
+
 window.events.on('finished_episode', function(model) {
-    history.appendEpisode(model);
     inventory.addKnowledge(model.pkg.brain.provides || []);
     var div = document.querySelector('.episode[name='+ model.pkg.name +']');
     appendMenu(div, TOC, history.visited(), inventory.knowledge(), TRACKS, TRACK_NAMES, 4); 
+    ensureVisible(div);
 });
 
 window.events.on('episode_chosen', function(e) {
@@ -77,5 +100,9 @@ window.events.on('episode_chosen', function(e) {
 
 function loadAndAppendEpisode(name) {
     var container = document.querySelector('.content');
-    episode.appendEpisode(container, TOC[name]);
+    episode.appendEpisode(container, TOC[name], function(err) {
+        if (err) return;
+        history.appendEpisode(TOC[name]);
+        scrollToEpisode(name);
+    });
 }
