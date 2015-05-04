fda9725154a0fdcf6f916299637d2d12e83087d5
Jan Bölsche <regular.gonzales@googlemail.com>
2015-05-04 22:39:01 +0200

    (slightly) improve code structure

--- app/app.js
+++ app/app.js
@@ -1,73 +1,22 @@
+// third-party
 var _ = require('lodash');
+var Spinner = require('spin');
 
-var EventEmitter = require('events').EventEmitter;
-window.events = new EventEmitter();
-
+// third party (db)
 var levelup = require('levelup');
 var leveldown = require('level-js');
 var sublevel = require('level-sublevel');
-var history, logdb;
-var db = levelup('brainpm', { db: leveldown }, function() {
-    logdb = sublevel(db, 'log');
-    history = require('history/history')('.sidebar .history ul', logdb);
-});
 
-var inventory = require('inventory/inventory')('.sidebar .inventory ul');
-var topbar = require('topbar/topbar')('.topbar');
-var discover = require('discover/discover');
-var episode = require('episode/episode');
-var appendMenu = require('menu/menu').appendMenu;
-var bpm = require('brainpm');
-var Spinner = require('spin');
-
+// helpers
 var scrollToY = function(y) {
     require('scroll-to-y')(y, 1500, 'easeInOutQuint');
 };
-
-var Spinner = require('spin');
-
-var TOC = {};
-var TRACKS = [];
-var TRACK_NAMES = [];
-
-function configure() {
-    var tracks = process.env.tracks.split('::');
-    console.log(tracks);
-    _.forEach(tracks, function(t) {
-        TRACKS.push(t.split(':')[0]);
-        TRACK_NAMES.push(t.split(':')[1]);
-    });
-}
-configure();
-
-var discoverySpinner = null;
-window.events.on('start_episode_discovery', function() {
-    discoverySpinner = new Spinner({color:'#111', lines: 12});
-    var container = document.querySelector('.container');
-    discoverySpinner.spin(container);
-});
-window.events.on('end_episode_discovery', function() {
-    discoverySpinner.stop();
-});
-
-window.events.once('end_episode_discovery', function(toc) {
-    TOC = toc;
-    console.log('episode discovery done.');
-    loadAndAppendEpisode('intro');
-    //loadAndAppendEpisode('tty');
-    //loadAndAppendEpisode('terminal-emulator');
-});
 
 function scrollToEpisode(name) {
     var el = document.querySelector('.episode[name=' + name + ']');
     var ypos = el.offsetTop;
     scrollToY(ypos); 
 }
-
-window.events.on('history_clicked', function(episode) {
-    console.log('history click on', episode.pkg.name);
-    scrollToEpisode(episode.pkg.name);
-});
 
 function ensureVisible(el) {
     var bounds = el.getBoundingClientRect();
@@ -84,34 +33,128 @@ function ensureVisible(el) {
     }
 }
 
-window.events.on('finished_episode', function(model) {
-    logdb.put('finished!'+(new Date()).toISOString(), model.pkg.name + '@' + model.pkg.version, function(err) {
-        console.log('PUT', err);
-    }); 
-    inventory.addKnowledge(model.pkg.brain.provides || []);
-    var div = document.querySelector('.episode[name='+ model.pkg.name +']');
-    appendMenu(div, TOC, history.visited(), inventory.knowledge(), TRACKS, TRACK_NAMES, 4); 
-    ensureVisible(div);
-});
+function init() {
+    var EventEmitter = require('events').EventEmitter;
+    window.events = new EventEmitter();
+    //
+    // app-specific modules
+    var discover = require('discover/discover');
+    var episode = require('episode/episode');
+    var appendMenu = require('menu/menu').appendMenu;
+    var bpm = require('brainpm');
+    
+    // app-specific UI components
+    var inventory = require('inventory/inventory')(
+            '.sidebar .inventory ul'
+    );
+    var topbar = require('topbar/topbar')('.topbar');
 
-window.events.on('episode_chosen', function(e) {
-    var model = e.episode;
-    console.log('user chose', model.pkg.name);
-    // is this a valid option?
-    var options = bpm.getOptions(TOC, inventory.knowledge());
-    if (_.some(options, function(o) {return o.pkg.name === model.pkg.name;})) {
-        e.menu.parentElement.removeChild(e.menu);
-        loadAndAppendEpisode(model.pkg.name);
-    } else {
-       // TODO: display 'you are not ready yet' 
+    function loadAndAppendEpisode(model) {
+        var container = document.querySelector('.content');
+        episode.appendEpisode(
+            container, model, 
+            function(err) {
+                if (err) return;
+                history.appendEpisode(model);
+                scrollToEpisode(model.pkg.name);
+            }
+        );
     }
-});
 
-function loadAndAppendEpisode(name) {
-    var container = document.querySelector('.content');
-    episode.appendEpisode(container, TOC[name], function(err) {
-        if (err) return;
-        history.appendEpisode(TOC[name]);
-        scrollToEpisode(name);
+    var TOC = {};
+    var TRACKS = [];
+    var TRACK_NAMES = [];
+    var history, logdb;
+    var discoverySpinner = null;
+
+    var tracks = process.env.tracks.split('::');
+    console.log(tracks);
+    _.forEach(tracks, function(t) {
+        TRACKS.push(t.split(':')[0]);
+        TRACK_NAMES.push(t.split(':')[1]);
+    });
+
+    var db = levelup('brainpm', {
+        db: leveldown
+    }, function() {
+        logdb = sublevel(db, 'log');
+        history = require('history/history')(
+            '.sidebar .history ul', 
+            logdb
+        );
+    });
+    
+    //
+    // -- add event listeners
+    //
+    var e = window.events;
+
+    e.on('start_episode_discovery', function() {
+        discoverySpinner = new Spinner({
+            color:'#111', 
+            lines: 12
+        });
+        var container = document.querySelector('.container');
+        discoverySpinner.spin(container);
+    });
+
+    e.on('end_episode_discovery', function() {
+        discoverySpinner.stop();
+    });
+
+    e.once('end_episode_discovery', function(toc) {
+        TOC = toc;
+        console.log('episode discovery done.');
+        loadAndAppendEpisode(TOC.intro);
+    });
+
+    e.on('history_clicked', function(episode) {
+        console.log('history click on', episode.pkg.name);
+        scrollToEpisode(episode.pkg.name);
+    });
+
+    e.on('finished_episode', function(model) {
+        logdb.put(
+            'finished!'+(new Date()).toISOString(), 
+            model.pkg.name + '@' + model.pkg.version, 
+            function(err) {
+                console.log('PUT', err);
+            })
+        ; 
+        inventory.addKnowledge(
+            model.pkg.brain.provides || []
+        );
+        var div = document.querySelector(
+            '.episode[name='+ model.pkg.name +']'
+        );
+        appendMenu(
+            div, 
+            TOC, 
+            history.visited(), 
+            inventory.knowledge(), 
+            TRACKS, TRACK_NAMES, 4
+        ); 
+        ensureVisible(div);
+    });
+
+    e.on('episode_chosen', function(e) {
+        var model = e.episode;
+        console.log('user chose', model.pkg.name);
+        // is this a valid option?
+        var options = bpm.getOptions(
+            TOC, 
+            inventory.knowledge()
+        );
+        if (_.some(options, function(o) {
+            return o.pkg.name === model.pkg.name;
+        })) {
+            e.menu.parentElement.removeChild(e.menu);
+            loadAndAppendEpisode(model);
+        } else {
+            // TODO: display 'you are not ready yet' 
+        }
     });
 }
+init();
+
+
